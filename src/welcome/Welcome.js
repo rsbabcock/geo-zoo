c3a0d40b60b21c41872284ec703285c85b21d0cc
rsbabcock <rachael.s.babcock@gmail.com>
2018-06-13 14:46:03 -0500

    welcome page instructions

--- src/welcome/Welcome.js
+++ src/welcome/Welcome.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { Columns, Column, Button } from 'bloomer'
+import { HeroFooter } from 'bloomer/lib/layout/Hero/HeroFooter';
+import { HeroFooter } from 'bloomer/lib/layout/Hero/HeroFooter';
+import { HeroHeader } from 'bloomer/lib/layout/Hero/HeroHeader';
 
 
 class Welcome extends Component {
@@ -15,7 +18,10 @@ class Welcome extends Component {
                     <Columns isCentered>
                         <Column isSize="1/2">
                             <h1> Welcome to GeoZoo</h1>
-                            <h3> Instructions </h3>
+                            <h3> To play,  click on the correct continent image for the animal. You only have one guess before it moves to the next animal, so make sure to read the facts before you guess!
+
+                            Have fun and good luck!
+                             </h3>
                             <Button id="play__game" onClick={this.props.showView}> Play </Button>
                         </Column>
                     </Columns >
